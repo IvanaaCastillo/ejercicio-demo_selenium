b9d0293e35b5cd9ce465e3da41c1aa2eef0c5867
Ivana Castillo <ivanaacastillo@gmail.com>
2025-07-17 22:08:11 -0400

    arreglo nuevamente de test

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,18 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-import tempfile
+from webdriver_manager.chrome import ChromeDriverManager
 
 def test_page_title():
-    chrome_options = Options()
-    temp_dir = tempfile.mkdtemp()  # Carpeta temporal para el perfil de usuario
-    chrome_options.add_argument(f"--user-data-dir={temp_dir}")
-    
-    browser = webdriver.Chrome(options=chrome_options)
-
+    service = Service(ChromeDriverManager().install())
+    browser = webdriver.Chrome(service=service)
     browser.get('https://github.com')
     titleElement = browser.find_element(By.ID, 'hero-section-brand-heading')
-
     assert titleElement.text == 'Build and ship software on a single, collaborative platform'
-
     browser.quit()
