6becead90b454e074d2627546ddaf9540b601d99
Ivana Castillo <ivanaacastillo@gmail.com>
2025-07-17 22:03:40 -0400

    detalles

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,9 +1,8 @@
 from selenium import webdriver 
-from selenium.webdriver.chrome.service import Service 
 from selenium.webdriver.common.by import By
 
 def test_page_title():
-    browser = webdriver.Chrome(service=Service("C:\\Program Files (x86)\\chromedriver\\chromedriver.exe"))
+    browser = webdriver.Chrome()
     browser.get('https://github.com')
 
     titleElement = browser.find_element(By.ID, 'hero-section-brand-heading')
