b02cf3bfb337d38c4f1a694ee120f366569e482e
Ivana Castillo <ivanaacastillo@gmail.com>
2025-07-17 22:05:55 -0400

    arreglo de archivo test

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,11 +1,18 @@
-from selenium import webdriver 
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import tempfile
 
 def test_page_title():
-    browser = webdriver.Chrome()
-    browser.get('https://github.com')
+    chrome_options = Options()
+    temp_dir = tempfile.mkdtemp()  # Carpeta temporal para el perfil de usuario
+    chrome_options.add_argument(f"--user-data-dir={temp_dir}")
+    
+    browser = webdriver.Chrome(options=chrome_options)
 
+    browser.get('https://github.com')
     titleElement = browser.find_element(By.ID, 'hero-section-brand-heading')
 
     assert titleElement.text == 'Build and ship software on a single, collaborative platform'
+
     browser.quit()
