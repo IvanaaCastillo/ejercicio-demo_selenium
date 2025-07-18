990f9cb27c13162b3a622b911367c92ea399cbae
Ivana Castillo <ivanaacastillo@gmail.com>
2025-07-17 21:36:47 -0400

    first commit

---
+++ tests/first_test.py
@@ -0,0 +1,12 @@
+from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service 
+from selenium.webdriver.common.by import By
+
+def test_page_title():
+    browser = webdriver.Chrome(service=Service("C:\\Program Files (x86)\\chromedriver\\chromedriver.exe"))
+    browser.get('https://github.com')
+
+    titleElement = browser.find_element(By.ID, 'hero-section-brand-heading')
+
+    assert titleElement.text == 'Build and ship software on a single, collaborative platform'
+    browser.quit()
